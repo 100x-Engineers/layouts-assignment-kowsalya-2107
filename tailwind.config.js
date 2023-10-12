a121811dff4ba18016cc4a8aa30299372ac7df3e
kowsalya-2107 <142567225+kowsalya-2107@users.noreply.github.com>
2023-10-12 18:32:48 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,31 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "neutral-50": "#F9F9F9",
+        "neutral-1000": "#000000",
+        "twitter-blue": "#1D9BF0",
+        "neutral-700": "#404040",
+        "stroke": "#1D9BF0",
+        "stroke": "#FFFFFF",
+        "pink-500":"#E91E63",
+        "gray-200":"#EEEEEE",
+        "green-700":"#388E3C",
+      },
+      spacing: {
+        "gap": "100px",
+      },
+      fontFamily: {
+        'Inter': ['sans-serif'],
+      },
+      
+      
+     
+  
+    },
   },
   plugins: [],
-};
+}
+
